ad13f10fa8a39179b61bf36df80c75cccec17015
Rosty Kerei <rosty.kerei@nl.ibm.com>
2017-10-14 21:43:22 +0200

    tables ddl

--- server/src/main/java/nl/rostykerei/planes/server/model/Aircraft.java
+++ server/src/main/java/nl/rostykerei/planes/server/model/Aircraft.java
@@ -4,9 +4,8 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "aircraft", indexes = {
-        @Index(name = "aircraft_registration_idx", columnList = "registration"),
         @Index(name = "aircraft_airline_code_idx", columnList = "airline_code"),
-        @Index(name = "aircraft_selcal_idx", columnList = "selcal"),
+        @Index(name = "aircraft_reg_compact_idx", columnList = "reg_compact"),
         @Index(name = "aircraft_type_idx", columnList = "type"),
 })
 public class Aircraft {
@@ -19,11 +18,11 @@ public class Aircraft {
     @JoinColumn(name = "airline_code", foreignKey = @ForeignKey(name = "aircraft_airline_fk"))
     private Airline airline;
 
-    @Column(name = "registration")
+    @Column(name = "reg", length = 16)
     private String registration;
 
-    @Column(name = "selcal")
-    private String selcal;
+    @Column(name = "reg_compact", length = 16)
+    private String registrationCompact;
 
     @ManyToOne
     @JoinColumn(name = "type", foreignKey = @ForeignKey(name = "aircraft_type_fk"))
--- server/src/main/resources/db/migration/V0007__aircraft-ddl.sql
+++ server/src/main/resources/db/migration/V0007__aircraft-ddl.sql
@@ -2,8 +2,8 @@ CREATE TABLE aircraft
 (
   code         VARCHAR(6)   NOT NULL PRIMARY KEY,
   airline_code VARCHAR(3)   NULL,
-  registration VARCHAR(255) NULL,
-  selcal       VARCHAR(255) NULL,
+  reg          VARCHAR(16)  NULL,
+  reg_compact  VARCHAR(16)  NULL,
   type         VARCHAR(4)   NULL,
   model        VARCHAR(255) NULL,
   CONSTRAINT aircraft_type_fk FOREIGN KEY (type) REFERENCES aircraft_type (type),
@@ -12,9 +12,7 @@ CREATE TABLE aircraft
 
 CREATE INDEX aircraft_airline_code_idx ON aircraft (airline_code);
 
-CREATE INDEX aircraft_registration_idx ON aircraft (registration);
-
-CREATE INDEX aircraft_selcal_idx ON aircraft (selcal);
+CREATE INDEX aircraft_reg_compact_idx ON aircraft (reg_compact);
 
 CREATE INDEX aircraft_type_idx ON aircraft (type);
 
