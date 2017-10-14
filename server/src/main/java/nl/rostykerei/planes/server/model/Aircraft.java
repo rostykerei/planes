2541c7d1bb88646b4592d8ba2d44eec650cfa5f0
Rosty Kerei <rosty.kerei@nl.ibm.com>
2017-10-14 21:39:08 +0200

    tables ddl

--- server/src/main/java/nl/rostykerei/planes/server/model/Aircraft.java
+++ server/src/main/java/nl/rostykerei/planes/server/model/Aircraft.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "aircraft", indexes = {
         @Index(name = "aircraft_registration_idx", columnList = "registration"),
+        @Index(name = "aircraft_airline_code_idx", columnList = "airline_code"),
         @Index(name = "aircraft_selcal_idx", columnList = "selcal"),
         @Index(name = "aircraft_type_idx", columnList = "type"),
 })
@@ -13,6 +14,10 @@ public class Aircraft {
     @Id
     @Column(name = "code", length = 6, nullable = false, unique = true)
     private String code;
+
+    @ManyToOne
+    @JoinColumn(name = "airline_code", foreignKey = @ForeignKey(name = "aircraft_airline_fk"))
+    private Airline airline;
 
     @Column(name = "registration")
     private String registration;
--- server/src/main/resources/db/migration/V0007__aircraft-ddl.sql
+++ server/src/main/resources/db/migration/V0007__aircraft-ddl.sql
@@ -1,12 +1,16 @@
 CREATE TABLE aircraft
 (
   code         VARCHAR(6)   NOT NULL PRIMARY KEY,
-  model        VARCHAR(255) NULL,
+  airline_code VARCHAR(3)   NULL,
   registration VARCHAR(255) NULL,
   selcal       VARCHAR(255) NULL,
   type         VARCHAR(4)   NULL,
-  CONSTRAINT aircraft_type_fk FOREIGN KEY (type) REFERENCES aircraft_type (type)
+  model        VARCHAR(255) NULL,
+  CONSTRAINT aircraft_type_fk FOREIGN KEY (type) REFERENCES aircraft_type (type),
+  CONSTRAINT aircraft_airline_fk FOREIGN KEY (airline_code) REFERENCES airline (code)
 );
+
+CREATE INDEX aircraft_airline_code_idx ON aircraft (airline_code);
 
 CREATE INDEX aircraft_registration_idx ON aircraft (registration);
 
