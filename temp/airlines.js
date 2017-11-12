690eaf5662c493c1d2ff2cf10439aa7e41471da5
Rosty Kerei <rosty.kerei@nl.ibm.com>
2017-11-12 18:48:59 +0100

    remove temp stuff

--- temp/airlines.js
+++
@@ -1,8 +0,0 @@
-let airlines = require('./l1.json');
-let l2 = require('./l2.json');
-
-for (let icao in airlines) {
-
-    let callsign = l2.hasOwnProperty(icao) ? l2[icao].callsign : null;
-    console.log(icao + " " + callsign);
-}
