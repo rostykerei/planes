d99cf106d4a1c5cb0825db204d98102ef33fa2f3
Rosty Kerei <rosty.kerei@nl.ibm.com>
2017-11-16 12:42:34 +0100

    tables added

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import {
   MatButtonModule, MatCardModule, MatGridListModule, MatIconModule, MatInputModule, MatProgressSpinnerModule,
+  MatTableModule,
   MatTabsModule,
   MatToolbarModule
 } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatGridListModule, MatIconModule],
-  exports: [MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatGridListModule, MatIconModule],
+  imports: [MatButtonModule, MatToolbarModule, MatGridListModule, MatTableModule, MatCardModule],
+  exports: [MatButtonModule, MatToolbarModule, MatGridListModule, MatTableModule, MatCardModule],
 })
 export class MaterialModule { }
