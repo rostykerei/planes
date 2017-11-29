c8c7e88e895964d01722f2b67ba80abed60fbd59
Rosty Kerei <rosty.kerei@nl.ibm.com>
2017-11-29 11:40:37 +0100

    dashboard update

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, Component} from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import {MapService} from "../map.service";
+import {MapFlight} from "../model/map-flight";
 
 @Component({
   selector: 'app-dashboard',
@@ -10,17 +11,25 @@ import {MapService} from "../map.service";
 export class DashboardComponent implements AfterViewInit {
 
   map: mapboxgl.Map;
+  popup: mapboxgl.Popup;
 
-  layers : any = {};
+  flights : any = {};
+  drawnFlights = new Set();
 
   constructor(private mapService: MapService) {
+
+    this.popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false
+    });
+
   }
 
   ngAfterViewInit(): void {
     mapboxgl.accessToken = 'pk.eyJ1Ijoicm9zdHlrZXJlaSIsImEiOiJjaXVpdXk1enowMDNwMnlwcjFicTgyZG5jIn0.JFWHhjOLeynWdzP-Xyojww';
 
     this.map = new mapboxgl.Map({
-      container: 'map', // container id
+      container: 'map',
       style: 'mapbox://styles/mapbox/light-v9',
       zoom: 7,
       center: [4.764167, 52.308056]
@@ -28,65 +37,88 @@ export class DashboardComponent implements AfterViewInit {
 
   }
 
+  flightsLoaded(flights : MapFlight[]) {
+    this.flights = {};
 
-  testBtnClick() {
-
-    this.mapService.getActiveFlights().forEach(ff => {
-      ff.forEach(f => {
-
-        this.layers["f" + f.id] = true;
-
-        if (this.map.getSource("f" + f.id)) {
-          this.map.getSource("f" + f.id).setData({
-            "type": "Point",
-            "coordinates": [f.lon, f.lat]
-          });
-          this.map.setLayoutProperty("f" + f.id, 'icon-rotate', f.heading || 0);
-        }
-        else {
-          this.map.addSource("f" + f.id, {type: 'geojson',
-            data: {
-              "geometry": {
-                "type": "Point",
-                "coordinates": [f.lon, f.lat]},
-              "type": "Feature",
-              "properties": {}
-            }
-          });
-
-          this.map.addLayer({
-            "id": "f" + f.id,
-            "type": "symbol",
-            "source": "f" +  f.id,
-            "layout": {
-              "icon-image": "airport-15",
-              "icon-rotation-alignment": "map",
-              "icon-rotate": f.heading || 0
-            }
-          });
-
-          this.map.on('click', "f" + f.id, (e) => {
-            this.map.flyTo({center: e.lngLat});
-            new mapboxgl.Popup()
-              .setLngLat(e.lngLat)
-              .setHTML(e.toString())
-              .addTo(this.map);
-          });
-
-          this.map.on('mouseenter', "f" + f.id, () => {
-            this.map.getCanvas().style.cursor = 'pointer';
-          });
-
-          // Change it back to a pointer when it leaves.
-          this.map.on('mouseleave', "f" + f.id, () => {
-            this.map.getCanvas().style.cursor = '';
-          });
-
-        }
-      });
+    flights.forEach(f => {
+      this.flights[f.id] = f;
     });
 
-    /* this.map.removeSource('drone');
-    this.map.removeLayer('drone'); */
+    this.updateMap();
+  }
+
+  updateMap() {
+    // Delete disappeared flights
+    this.drawnFlights.forEach(f => {
+      if (!this.flights.hasOwnProperty(f)) {
+        this.map.removeSource('f' + f);
+        this.map.removeLayer('f' + f);
+      }
+    });
+
+    for (let id in this.flights) {
+      this.updateFlight(this.flights[id]);
+    }
+  }
+
+  updateFlight(f : MapFlight) {
+    if (this.map.getSource("f" + f.id)) {
+
+      this.map.getSource("f" + f.id).setData({
+        "type": "Point",
+        "coordinates": [f.lon, f.lat]
+      });
+
+      this.map.setLayoutProperty("f" + f.id, 'icon-rotate', f.heading || 0);
+    }
+    else {
+      this.map.addSource("f" + f.id, {type: 'geojson',
+        data: {
+          "geometry": {
+            "type": "Point",
+            "coordinates": [f.lon, f.lat]},
+          "type": "Feature",
+          "properties": {}
+        }
+      });
+
+      this.map.addLayer({
+        "id": "f" + f.id,
+        "type": "symbol",
+        "source": "f" +  f.id,
+        "layout": {
+          "icon-image": "airport-15",
+          "icon-rotation-alignment": "map",
+          "icon-rotate": f.heading || 0
+        }
+      });
+
+      this.map.on('click', "f" + f.id, (e) => {
+        this.map.flyTo({center: e.lngLat});
+        // todo
+      });
+
+      this.map.on('mouseenter', "f" + f.id, (e) => {
+        let id : number = parseInt(e.features[0].layer.id.substring(1));
+
+
+        this.map.getCanvas().style.cursor = 'pointer';
+        this.popup.setLngLat(e.lngLat)
+          .setHTML(this.flights[id].type)
+          .addTo(this.map);
+      });
+
+      // Change it back to a pointer when it leaves.
+      this.map.on('mouseleave', "f" + f.id, () => {
+        this.map.getCanvas().style.cursor = '';
+        this.popup.remove();
+      });
+
+    }
+  }
+
+
+  testBtnClick() {
+    this.mapService.getActiveFlights().subscribe(f => this.flightsLoaded(f));
   }
 }
