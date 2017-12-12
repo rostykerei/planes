6b157fa4ab29a5149c1035f9f824a6619f91934f
Rosty Kerei <rosty.kerei@gmail.com>
2017-12-12 22:09:30 +0100

    details update

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -2,14 +2,14 @@ import {NgModule} from '@angular/core';
 import {
   MatButtonModule,
   MatCardModule,
-  MatGridListModule,
+  MatGridListModule, MatIconModule,
   MatTableModule,
   MatToolbarModule,
   MatTooltipModule
 } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatGridListModule, MatTableModule, MatCardModule, MatTooltipModule],
-  exports: [MatButtonModule, MatToolbarModule, MatGridListModule, MatTableModule, MatCardModule, MatTooltipModule],
+  imports: [MatButtonModule, MatToolbarModule, MatGridListModule, MatTableModule, MatCardModule, MatTooltipModule, MatIconModule],
+  exports: [MatButtonModule, MatToolbarModule, MatGridListModule, MatTableModule, MatCardModule, MatTooltipModule, MatIconModule],
 })
 export class MaterialModule { }
