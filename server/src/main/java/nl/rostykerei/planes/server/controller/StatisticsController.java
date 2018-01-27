d0e5fc81e8560742ffb971fd798c0f2e51f5014a
Rosty Kerei <rosty.kerei@gmail.com>
2018-01-27 14:20:51 +0100

    add airlines stats

--- server/src/main/java/nl/rostykerei/planes/server/controller/StatisticsController.java
+++ server/src/main/java/nl/rostykerei/planes/server/controller/StatisticsController.java
@@ -4,6 +4,7 @@ import nl.rostykerei.planes.server.repository.StatisticsRepository;
 import nl.rostykerei.planes.server.repository.vo.NameValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,17 @@ import java.util.List;
 @RequestMapping("/stats")
 public class StatisticsController {
 
+    private final static Pageable PAGE_REQUEST = PageRequest.of(0, 10);
     @Autowired
     private StatisticsRepository repository;
 
     @RequestMapping("/aircrafts")
     public List<NameValue> getTopAircrafts() {
-        return repository.getTopAirlines(PageRequest.of(0, 10)).getContent();
+        return repository.getTopAircrafts(PAGE_REQUEST).getContent();
+    }
+
+    @RequestMapping("/airlines")
+    public List<NameValue> getTopAirlines() {
+        return repository.getTopAirlines(PAGE_REQUEST).getContent();
     }
 }
--- server/src/main/java/nl/rostykerei/planes/server/repository/StatisticsRepository.java
+++ server/src/main/java/nl/rostykerei/planes/server/repository/StatisticsRepository.java
@@ -11,9 +11,17 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StatisticsRepository extends JpaRepository<Aircraft, Integer> {
 
-    @Query("SELECT NEW nl.rostykerei.planes.server.repository.vo.NameValue(a.type.type, count(a)) " +
+    @Query("SELECT NEW nl.rostykerei.planes.server.repository.vo.NameValue(a.type.type, COUNT(a)) " +
             "FROM Flight f JOIN Aircraft a ON f.aircraft = a.code " +
             "WHERE a.type IS NOT NULL " +
             "GROUP BY a.type ORDER BY count(a) DESC")
+    Page<NameValue> getTopAircrafts(Pageable pageable);
+
+    @Query("SELECT NEW nl.rostykerei.planes.server.repository.vo.NameValue(al.code, COUNT(al)) " +
+            "FROM Flight f " +
+            "JOIN Aircraft a ON f.aircraft = a.code " +
+            "JOIN Airline al ON a.airline = al.code " +
+            "WHERE a.airline IS NOT NULL " +
+            "GROUP BY al.code ORDER BY count(al) DESC")
     Page<NameValue> getTopAirlines(Pageable pageable);
 }
