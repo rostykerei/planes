9f1ae247817b104347c0c514244ac331aca7936d
Rosty Kerei <rosty.kerei@gmail.com>
2018-01-27 16:00:37 +0100

    stats template added

--- frontend/src/app/aircrafts/aircrafts.component.ts
+++ frontend/src/app/aircrafts/aircrafts.component.ts
@@ -7,24 +7,6 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AircraftsComponent implements OnInit {
 
-  pieChartData =  {
-    chartType: 'PieChart',
-    dataTable: [
-      ['Aircraft', 'Flights in the last 24 hours'],
-      ['B738',     1597],
-      ['A320',      1056],
-      ['A319',  685],
-      ['E190', 442],
-      ['A321', 363],
-      ['B737', 268],
-      ['B77W', 197],
-      ['E75L', 161],
-      ['DH8D', 144],
-      ['B772',    105]
-    ],
-    options: {'title': 'Aircrafts'},
-  };
-
   constructor() { }
 
   ngOnInit() {
--- frontend/src/app/stats/stats.component.ts
+++ frontend/src/app/stats/stats.component.ts
@@ -7,7 +7,108 @@ import {Component, OnInit} from '@angular/core';
 })
 export class StatsComponent implements OnInit {
 
-  constructor() { }
+  aircraftsChart = {
+    chartType: 'PieChart',
+    dataTable: [
+      ['Aircraft', 'Flights'],
+      ['B738', Math.random() * 1000],
+      ['A320', Math.random() * 1000],
+      ['A319', Math.random() * 1000],
+      ['E190', Math.random() * 1000],
+      ['A321', Math.random() * 1000],
+      ['B737', Math.random() * 1000],
+      ['B77W', Math.random() * 1000],
+      ['E75L', Math.random() * 1000],
+      ['DH8D', Math.random() * 1000],
+      ['B772', Math.random() * 1000],
+    ],
+    options: {
+      title: 'Top Aircrafts', height: 280, legend: 'right',
+      titleTextStyle: {
+        fontName: 'Roboto',
+        fontSize: 24,
+        bold: false
+      }
+    }
+  };
+
+  airlinesChart = {
+    chartType: 'PieChart',
+    dataTable: [
+      ['Airline', 'Flights'],
+      ['KLM', Math.random() * 1000],
+      ['RYR', Math.random() * 1000],
+      ['BAW', Math.random() * 1000],
+      ['EZY', Math.random() * 1000],
+      ['KLC', Math.random() * 1000],
+      ['WZZ', Math.random() * 1000],
+      ['DLH', Math.random() * 1000],
+      ['BEE', Math.random() * 1000],
+      ['AFR', Math.random() * 1000],
+      ['TRA', Math.random() * 1000],
+    ],
+    options: {
+      title: 'Top Airlines', height: 280, legend: 'right',
+      titleTextStyle: {
+        fontName: 'Roboto',
+        fontSize: 24,
+        bold: false
+      }
+    }
+  };
+
+  originsChart = {
+    chartType: 'PieChart',
+    dataTable: [
+      ['Airport', 'Flights'],
+      ['EHAM', Math.random() * 1000],
+      ['EGLL', Math.random() * 1000],
+      ['EGSS', Math.random() * 1000],
+      ['EGCC', Math.random() * 1000],
+      ['EGGW', Math.random() * 1000],
+      ['EDDF', Math.random() * 1000],
+      ['EBBR', Math.random() * 1000],
+      ['EDDM', Math.random() * 1000],
+      ['LFPG', Math.random() * 1000],
+      ['EIDW', Math.random() * 1000],
+    ],
+    options: {
+      title: 'Top Origins', height: 280, legend: 'right',
+      titleTextStyle: {
+        fontName: 'Roboto',
+        fontSize: 24,
+        bold: false
+      }
+    }
+  };
+
+  destinationsChart = {
+    chartType: 'PieChart',
+    dataTable: [
+      ['Airport', 'Flights'],
+      ['EHAM', Math.random() * 1000],
+      ['EGLL', Math.random() * 1000],
+      ['EGSS', Math.random() * 1000],
+      ['EGGW', Math.random() * 1000],
+      ['EIDW', Math.random() * 1000],
+      ['EGKK', Math.random() * 1000],
+      ['EKCH', Math.random() * 1000],
+      ['EGCC', Math.random() * 1000],
+      ['ESSA', Math.random() * 1000],
+      ['EGBB', Math.random() * 1000],
+    ],
+    options: {
+      title: 'Top Destinations', height: 280, legend: 'right',
+      titleTextStyle: {
+        fontName: 'Roboto',
+        fontSize: 24,
+        bold: false
+      }
+    },
+  };
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
