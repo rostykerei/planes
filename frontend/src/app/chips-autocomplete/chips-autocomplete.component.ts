91794741d461276c13a1706a224318d20cab4633
Rosty Kerei <rosty.kerei@gmail.com>
2018-01-30 21:36:03 +0100

    chips autocomplete init

--- frontend/src/app/chips-autocomplete/chips-autocomplete.component.ts
+++ frontend/src/app/chips-autocomplete/chips-autocomplete.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormControl} from "@angular/forms";
+import {MatAutocompleteSelectedEvent, MatAutocompleteTrigger} from "@angular/material";
 
 @Component({
   selector: 'app-chips-autocomplete',
@@ -7,9 +9,71 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ChipsAutocompleteComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('chipInput', {read: MatAutocompleteTrigger})
+  private autoCompleteTrigger: MatAutocompleteTrigger;
+  // Set up reactive formcontrol
+  autoCompleteChipList: FormControl = new FormControl();
+  // Set up values to use with Chips
+  selectable: boolean = true;
+  removable: boolean = true;
+  addOnBlur: boolean = true;
+  // Set up Options Array
+  options = [
+    {name: 'Lemon'},
+    {name: 'Lime'},
+    {name: 'Apple'},
+  ];
+  // Define filteredOptins Array and Chips Array
+  filteredOptions = [];
+  chips = [];
 
   ngOnInit() {
+    // Set initial value of filteredOptions to all Options
+    this.filteredOptions = this.options;
+    // Subscribe to listen for changes to AutoComplete input and run filter
+    this.autoCompleteChipList.valueChanges.subscribe(val => {
+      this.filterOptions(val);
+    })
+  }
+
+  inputFocus() {
+    setTimeout(() => {
+      if (!this.autoCompleteTrigger.panelOpen) {
+        this.autoCompleteTrigger.openPanel();
+      }
+    }, 10);
+  }
+
+  filterOptions(text: string) {
+    // Set filteredOptions array to filtered options
+    this.filteredOptions = this.options
+      .filter(obj => obj.name.toLowerCase().indexOf(text.toString().toLowerCase()) === 0);
+  }
+
+  addChip(event: MatAutocompleteSelectedEvent, input: any): void {
+    // Define selection constant
+    const selection = event.option.value;
+    // Add chip for selected option
+    this.chips.push(selection);
+    // Remove selected option from available options and set filteredOptions
+    this.options = this.options.filter(obj => obj.name !== selection.name);
+    this.filteredOptions = this.options;
+    // Reset the autocomplete input text value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  removeChip(chip: any): void {
+    // Find key of object in array
+    let index = this.chips.indexOf(chip);
+    // If key exists
+    if (index >= 0) {
+      // Remove key from chips array
+      this.chips.splice(index, 1);
+      // Add key to options array
+      this.options.push(chip);
+    }
   }
 
 }
--- frontend/src/app/flights-filter/flights-filter.component.ts
+++ frontend/src/app/flights-filter/flights-filter.component.ts
@@ -1,6 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
-import {MatAutocompleteSelectedEvent, MatAutocompleteTrigger} from "@angular/material";
-import {FormControl} from "@angular/forms";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-flights-filter',
@@ -9,71 +7,4 @@ import {FormControl} from "@angular/forms";
 })
 export class FlightsFilterComponent {
 
-  @ViewChild('chipInput', {read: MatAutocompleteTrigger})
-  private autoCompleteTrigger: MatAutocompleteTrigger;
-  // Set up reactive formcontrol
-  autoCompleteChipList: FormControl = new FormControl();
-  // Set up values to use with Chips
-  visible: boolean = true;
-  selectable: boolean = true;
-  removable: boolean = true;
-  addOnBlur: boolean = true;
-  // Set up Options Array
-  options = [
-    {name: 'Lemon'},
-    {name: 'Lime'},
-    {name: 'Apple'},
-  ];
-  // Define filteredOptins Array and Chips Array
-  filteredOptions = [];
-  chips = [];
-
-  ngOnInit() {
-    // Set initial value of filteredOptions to all Options
-    this.filteredOptions = this.options;
-    // Subscribe to listen for changes to AutoComplete input and run filter
-    this.autoCompleteChipList.valueChanges.subscribe(val => {
-      this.filterOptions(val);
-    })
-  }
-
-  inputFocus() {
-    setTimeout(() => {
-      if (!this.autoCompleteTrigger.panelOpen) {
-        this.autoCompleteTrigger.openPanel();
-      }
-    }, 10);
-  }
-
-  filterOptions(text: string) {
-    // Set filteredOptions array to filtered options
-    this.filteredOptions = this.options
-      .filter(obj => obj.name.toLowerCase().indexOf(text.toString().toLowerCase()) === 0);
-  }
-
-  addChip(event: MatAutocompleteSelectedEvent, input: any): void {
-    // Define selection constant
-    const selection = event.option.value;
-    // Add chip for selected option
-    this.chips.push(selection);
-    // Remove selected option from available options and set filteredOptions
-    this.options = this.options.filter(obj => obj.name !== selection.name);
-    this.filteredOptions = this.options;
-    // Reset the autocomplete input text value
-    if (input) {
-      input.value = '';
-    }
-  }
-
-  removeChip(chip: any): void {
-    // Find key of object in array
-    let index = this.chips.indexOf(chip);
-    // If key exists
-    if (index >= 0) {
-      // Remove key from chips array
-      this.chips.splice(index, 1);
-      // Add key to options array
-      this.options.push(chip);
-    }
-  }
 }
