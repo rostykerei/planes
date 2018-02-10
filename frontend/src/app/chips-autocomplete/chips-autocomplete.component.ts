61b73295c567ec883c8d8119adb981be2bc93b6e
Rosty Kerei <rosty.kerei@gmail.com>
2018-02-10 22:38:38 +0100

    flights filter try

--- frontend/src/app/chips-autocomplete/chips-autocomplete.component.ts
+++ frontend/src/app/chips-autocomplete/chips-autocomplete.component.ts
@@ -15,81 +15,52 @@ export class ChipsAutocompleteComponent implements OnInit {
 
   @Input() private title: string;
 
-  mymodel: string = '';
-
   // Set up reactive formcontrol
   autoCompleteChipList: FormControl = new FormControl();
 
   color = 'warn';
 
-  // Set up Options Array
-  options = [
-  ];
-  // Define filteredOptins Array and Chips Array
-  filteredOptions = [];
-  chips = [];
+  options = new Set();
 
+  chips = new Set();
 
   constructor(private autoCompleteService: AutocompleteService) {
   }
 
   ngOnInit() {
-    // Set initial value of filteredOptions to all Options
-    this.filteredOptions = this.options;
-    // Subscribe to listen for changes to AutoComplete input and run filter
-    /*this.autoCompleteChipList.valueChanges.subscribe(val => {
-      this.filterOptions(val);
-    });*/
-
     this.autoCompleteChipList.valueChanges
       .subscribe(val => {
         if (val) {
           this.autoCompleteService.getAirports(val)
             .subscribe(res => {
-              this.filteredOptions = [];
+              this.options.clear();
 
               res.forEach(a => {
-                this.filteredOptions.push(a);
+                if (!this.chips.has(a)) {
+                  this.options.add(a);
+                }
               });
             });
         }
         else {
-          this.filteredOptions = [];
+          this.options.clear();
         }
       })
-  }
-
-  inputFocus() {
-/*    setTimeout(() => {
-      if (!this.autoCompleteTrigger.panelOpen) {
-        this.autoCompleteTrigger.openPanel();
-      }
-    }, 10);*/
-  }
-
-  valuechange(newValue) {
-    //this.mymodel = newValue;
-    console.log(newValue)
   }
 
   inputBlur(input: any) {
     input.value = '';
   }
 
-  filterOptions(text: string) {
-    // Set filteredOptions array to filtered options
-    this.filteredOptions = this.options
-      .filter(obj => obj.name.toLowerCase().indexOf(text.toString().toLowerCase()) === 0);
-  }
-
   addChip(event: MatAutocompleteSelectedEvent, input: any): void {
     // Define selection constant
     const selection = event.option.value;
     // Add chip for selected option
-    this.chips.push(selection);
+    this.chips.add(selection);
+
     // Remove selected option from available options and set filteredOptions
-    this.options = this.options.filter(obj => obj.name !== selection.name);
-    this.filteredOptions = this.options;
+    ////this.options = this.options.filter(obj => obj.name !== selection.name);
+
     // Reset the autocomplete input text value
     if (input) {
       input.value = '';
@@ -97,15 +68,7 @@ export class ChipsAutocompleteComponent implements OnInit {
   }
 
   removeChip(chip: any): void {
-    // Find key of object in array
-    let index = this.chips.indexOf(chip);
-    // If key exists
-    if (index >= 0) {
-      // Remove key from chips array
-      this.chips.splice(index, 1);
-      // Add key to options array
-      this.options.push(chip);
-    }
+    this.chips.delete(chip);
   }
 
 }
