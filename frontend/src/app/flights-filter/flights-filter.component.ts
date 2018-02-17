542a29a3102deea15db286be9061a5dbf4825b82
Rosty Kerei <rosty.kerei@gmail.com>
2018-02-17 13:30:30 +0100

    store filter to query-params

--- frontend/src/app/flights-filter/flights-filter.component.ts
+++ frontend/src/app/flights-filter/flights-filter.component.ts
@@ -1,14 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-flights-filter',
   templateUrl: './flights-filter.component.html',
   styleUrls: ['./flights-filter.component.scss']
 })
-export class FlightsFilterComponent {
+export class FlightsFilterComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    console.log(this.route.snapshot.queryParams);
+  }
 
   change(field: string, value: Set<string>) {
-    console.log(field);
-    console.log(value);
+    let queryParams = {};
+    queryParams[field] = Array.from(value.values());
+
+    this.router.navigate([], { queryParams: queryParams, queryParamsHandling: 'merge' });
   }
 }
