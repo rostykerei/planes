d4db18684e07785bbdf88bd8d6514e407ae4f093
Rosty Kerei <rosty.kerei@gmail.com>
2018-03-04 21:06:38 +0100

    frontend update

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
-  production: true,
-  apiUrl: 'http://localhost:8080/',
+  production: false,
+  apiUrl: 'https://radar-api.rostykerei.nl/',
   mapStartLat: 52.4892462,
   mapStartLon: 4.749578,
   mapStartZoom: 7,
   mapTypeId: 'terrain'
 };
+
